e09fcc30e8a2dc939848002fec057b9f29fd0423
h2hyun37 <h2hyun37@gmail.com>
2016-03-25 19:43:20 +0900

    controller 에서 string return 으로 html 파일로 view 호출 가능하도록 수정 : spring-boot-starter-thymeleaf 필요

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -3,24 +3,44 @@ package hello;
 import model.User;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
-@RequestMapping(value = "/oauth")
-@EnableAutoConfiguration
+// @RequestMapping(value = "/oauth")
+@SpringBootApplication
 public class SampleController {
 
-	@RequestMapping("/")
+	@RequestMapping("/1")
 	// @ResponseBody
-	String home() {
+	String home(Model model) {
 
-		System.out.println("in home()");
+		System.out.println("in home() - 2");
+
+		// ModelAndView mav = new ModelAndView();
+		// mav.setViewName("hello");
+
+		// return mav;
 		return "hello"; // jsp?
 	}
+
+
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+
+		System.out.println("in greeting() - 2");
+
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
 
 	@RequestMapping(value = "/{userNumber}", method = RequestMethod.GET)
 	public User getUser(@PathVariable Long userNumber) {
